d61ecf9f7d1b1fbe5814a0b1cd699f99f1d0c87f
Javier Ramirez <javierm.ramirezp@gmail.com>
2021-10-22 18:23:02 -0500

    add headers

--- Proyecto 1 redes/src/Sockets/Server.java
+++ Proyecto 1 redes/src/Sockets/Server.java
@@ -1,8 +1,13 @@
 package Sockets;
 
+
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 
 
@@ -33,14 +38,33 @@ public class Server {
 
                 InputStream input = socket.getInputStream();
 
-                leerSolicitud(input);
+                HttpClient client = HttpClient.newHttpClient();
+                String headerRequest = leerSolicitud(input);
+
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI("http://info.cern.ch/"))
+                        //.header("Host", "info.cern.ch") - : restricted header name: "Connection"
+                        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0")
+                        .header("Accept"," text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+                        .header("Accept-Language", "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3")
+                        .header("Accept-Encoding", "gzip, deflate")
+                        //.header("Connection", "keep-alive") - : restricted header name: "Connection"
+                        .header("Upgrade-Insecure-Requests", "1")
+                        .GET()
+                        .build();
+
+                System.out.println(request.headers().toString());
+
+                
+
+
 
                 output = new DataOutputStream(socket.getOutputStream());
                 output.writeUTF("adios");
                 socket.close();
             }
 
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
     }
